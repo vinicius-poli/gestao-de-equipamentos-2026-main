5d0d5306909060a82c5757d4e1213cf7e44a3f02
Vinicius Poli <alemaumvp@hotmail.com>
2026-04-09 14:51:07 -0300

    Adicionada a função cadastro

---
+++ GestaoDeEquipamentos.ConsoleApp/Equipamento.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GestaoDeEquipamentos.ConsoleApp;
+
+public class Equipamento
+{
+    public string id;    
+    public string nome;
+    public string fabricante;
+    public decimal precoAquisicao;
+    public DateTime dataFabricacao;    
+}
---
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -0,0 +1,98 @@
+﻿using GestaoDeEquipamentos.ConsoleApp;
+
+Equipamento? [] equipamentos = new Equipamento[100];
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("Gestão de Equipamentos");
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("1 - Cadastrar equipamento");
+    Console.WriteLine("2 - Editar equipamento");
+    Console.WriteLine("3 - Excluir equipamento");
+    Console.WriteLine("4 - Visualizar equipamentos");
+    Console.WriteLine("S - Sair");
+    Console.WriteLine("---------------------------------");
+    Console.Write("> ");
+    string? opcaoMenu = Console.ReadLine()?.ToUpper();
+
+    if (opcaoMenu == "S")
+    {
+        Console.Clear();
+        break;
+    }
+
+    if (opcaoMenu == "1")
+    {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Gestão de Equipamentos");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Cadastro de Equipamento");
+        Console.WriteLine("---------------------------------");
+
+        Equipamento novoEquipamento = new Equipamento();
+
+        do
+        {
+            Console.WriteLine("Digite o nome do equipamento: ");
+            novoEquipamento.nome = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(novoEquipamento.nome) && novoEquipamento.nome.Length > 3)
+            {
+                break;
+            }            
+        } while (true); 
+
+        do
+        {
+            Console.WriteLine("Digite o fabricante do equipamento: ");
+            novoEquipamento.fabricante = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(novoEquipamento.fabricante)
+            && novoEquipamento.fabricante.Length > 2)
+            {
+                break;
+            }            
+        } while (true);
+
+        Console.WriteLine("Digite o preço de aquisição do equipamento: ");
+        novoEquipamento.precoAquisicao = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Digite a data de fabricação do equipamento (DD/MM/AAAA): ");
+        novoEquipamento.dataFabricacao = Convert.ToDateTime(Console.ReadLine());
+
+        for (int i = 0; i < equipamentos.Length; i++)
+        {
+            Equipamento? e = equipamentos[i];
+
+            if (e == null)
+            {
+                equipamentos[i]= novoEquipamento;
+                break;
+            }
+        }
+
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine($"O registro \"{novoEquipamento.nome}\" foi cadastrado com sucesso!");
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("Digite ENTER para continuar...");
+    Console.ReadLine();
+
+    }
+
+    else if (opcaoMenu == "2")
+    {
+
+    }
+
+    else if (opcaoMenu == "3")
+    {
+
+    }
+
+    else if (opcaoMenu == "4")
+    {
+
+    }
+}
